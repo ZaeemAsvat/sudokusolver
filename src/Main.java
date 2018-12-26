a8c0cd6a74f0637d9af448a9cb175dc4b68dc434
zaeemasvat <z.asvat07@gmail.com>
2018-12-26 15:41:28 +0200

    Changed possibleSolutions from a 2D matrix of arfaylists to a 2D matrix of HashSets, more efficient. And finished untested method to remove trivial impossible solutions using values already filled in

--- src/Main.java
+++ src/Main.java
@@ -3,17 +3,18 @@ import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.HashSet;
 
 public class Main {
 
     private static final int boardWithAndHeight = 9;
     private static final int[][] board = new int[boardWithAndHeight][boardWithAndHeight];
+    private static final ArrayList<ArrayList<HashSet<Integer>>> possibleSolutions = new ArrayList<>(boardWithAndHeight);
 
     public static void main(String[] args) throws IOException {
 	// write your code here
 
-        ArrayList<ArrayList<ArrayList<Integer>>> possibleSolutions = new ArrayList<>();
-        fillPossibleSolutionsBoard(possibleSolutions);
+        fillPossibleSolutionsBoard();
 
         File file = new File("");
 
@@ -44,16 +45,59 @@ public class Main {
         }
     }
 
-    private static void fillPossibleSolutionsBoard (ArrayList<ArrayList<ArrayList<Integer>>> possibleSolutions) {
-
-        possibleSolutions = new ArrayList<>(boardWithAndHeight);
+    private static void fillPossibleSolutionsBoard () {
         for (int row = 0; row < boardWithAndHeight; row++) {
             possibleSolutions.set(row, new ArrayList<>(boardWithAndHeight));
             for (int col = 0; col < boardWithAndHeight; col++) {
-                possibleSolutions.get(row).set(col, new ArrayList<>(boardWithAndHeight));
+                possibleSolutions.get(row).set(col, new HashSet<>(boardWithAndHeight));
                 for (int i = 0; i < boardWithAndHeight; i++)
-                    possibleSolutions.get(row).get(col).set(i, i + 1);
+                    possibleSolutions.get(row).get(col).add(i + 1);
             }
         }
     }
+
+    private static void removeTrivalImpossibleSolutions () {
+
+        for (int row = 0; row < boardWithAndHeight; row++) {
+            for (int col = 0; col < boardWithAndHeight; col++) {
+
+                // if cell isn't blank (there is a number already filled in this cell)
+                if (board[row][col] != -1) {
+
+                    // remove this number from all possible solution sets of cells
+                    // in the same row as this cell
+                    for (int c = 0; c < boardWithAndHeight; c++)
+                        possibleSolutions.get(row).get(c).remove(board[row][col]);
+
+                    // remove this number from all possible solution sets of cells
+                    // in the same column as this cell
+                    for (int r = 0; r < boardWithAndHeight; r++)
+                        possibleSolutions.get(r).get(col).remove(board[row][col]);
+
+                    // remove this number from all possible solution sets of cells
+                    // in the same block as this cell
+                    SubRange thisBlockRange = getSubRange(row, col);
+                    for (int r = thisBlockRange.getStartRow(); r <= thisBlockRange.getEndRow(); r++)
+                        for (int c = thisBlockRange.getStartCol(); c <= thisBlockRange.getEndCol(); c++)
+                            possibleSolutions.get(r).get(c).remove(board[row][col]);
+
+                }
+            }
+        }
+    }
+
+    private static SubRange getSubRange (int row, int col) {
+        return new SubRange(roundDown(row, 3), roundUp(row, 3),
+                            roundDown(col, 3), roundUp(col, 3));
+    }
+
+    private static int roundDown (int num, int multipleBase) {
+        return (int) Math.floor((double) num/multipleBase) * multipleBase;
+    }
+
+    private static int roundUp (int num, int multipleBase) {
+        return (int) Math.ceil((double) num/multipleBase) * multipleBase;
+    }
+
 }
+
---
+++ src/SubRange.java
@@ -0,0 +1,43 @@
+public class SubRange {
+
+    private int startRow = -1, startCol = -1, endRow = -1, endCol = -1;
+
+    SubRange (int startR, int endR, int startC, int endC) {
+        startRow = startR;
+        endRow = endR;
+        startCol = startC;
+        endCol = endC;
+    }
+
+    public void setStartRow(int startRow) {
+        this.startRow = startRow;
+    }
+
+    public void setEndRow(int endRow) {
+        this.endRow = endRow;
+    }
+
+    public void setStartCol(int startCol) {
+        this.startCol = startCol;
+    }
+
+    public void setEndCol(int endCol) {
+        this.endCol = endCol;
+    }
+
+    public int getStartRow() {
+        return startRow;
+    }
+
+    public int getEndRow() {
+        return endRow;
+    }
+
+    public int getStartCol() {
+        return startCol;
+    }
+
+    public int getEndCol() {
+        return endCol;
+    }
+}
